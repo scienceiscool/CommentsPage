26e2dc92a75ce893a2c4296bbd5745b87ecbb08b
Kathy <kathysaad@gmail.com>
2015-04-12 21:24:18 -0700

    Add $ in front of a variable in app.js bc it holds a jQuery object

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var main = function () {
  "use strict";
 
  $(".comment-input button").on("click", function (event) {
+
   //console.log("Hello World!");
   //$(".comments").append("<p>this is a new comment</p>");
+
   var $new_comment = $("<p>"),
+     // $ in front of above variable bc it will hold a jQuery object
    comment_text = $(".comment-input input").val();
+
   $new_comment.text(comment_text);
   $(".comments").append($new_comment);
  });
