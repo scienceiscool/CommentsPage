5af769e50340ca085700b3e18777fec372fd23d2
Kathy <kathysaad@gmail.com>
2015-04-13 13:12:31 -0700

    Refactor code in app.js so it follows the DRY principle, fixed spacing/indentation issues

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
 var main = function () {
   "use strict";
 
+  var addCommentFromInputBox = function () {
+    // $ in front of variable below bc it will hold a jQuery object
+    var $new_comment;
+
+    // the following if statement fixes a bug
+    // it won't publish an empty comment now
+    if ($(".comment-input input").val() !== "") {
+      $new_comment = $("<p>").text($(".comment-input input").val());
+      // the following lines calling hide() and fadeIn() and in the next
+      // block of code allow for a comment to not just appear but fade in
+      $new_comment.hide();
+      $(".comments").append($new_comment);
+      $new_comment.fadeIn();
+      $(".comment-input input").val("");
+    }
+  };
+
   $(".comment-input button").on("click", function (event) {
+    addCommentFromInputBox();
+  });
 
-  //console.log("Hello World!");
-  //$(".comments").append("<p>this is a new comment</p>");
-
-  var $new_comment = $("<p>"),
-     // $ in front of above variable bc it will hold a jQuery object
-   comment_text = $(".comment-input input").val();
-
-  // the following if statement fixes a bug
-  // it won't publish an empty comment now
-  if ($(".comment-input input").val() !== "") {
-    $new_comment.text(comment_text);
-    // the following lines calling hide() and fadeIn() and in the next
-    // block of code allow for a comment to not just appear but fade in
-    $new_comment.hide();
-    $(".comments").append($new_comment);
-    $new_comment.fadeIn();
-    $(".comment-input input").val("");
-  }
- });
-
- // code below is an event listener that follows the same logic as above,
- // except it responds when the Enter key is pressed
- $(".comment-input input").on("keypress", function (event) {
-   if (event.keyCode === 13) {
-     if ($(".comment-input input").val() !== "") {
-       var $new_comment = $("<p>").text($(".comment-input input").val());
-       $new_comment.hide();
-       $(".comments").append($new_comment);
-       $new_comment.fadeIn();
-       $(".comment-input input").val("");
-       //console.log("keyCode: " + event.keyCode);
-     }
-   }
- });
+  $(".comment-input input").on("keypress", function (event) {
+    if (event.keyCode === 13) {
+      addCommentFromInputBox();
+    }
+  });
 };
 
 $(document).ready(main);
